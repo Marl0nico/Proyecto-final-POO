1518c9a2750f90d399e1de4d8479e672bd94034e
Marl0nico <marlon.nicolalde@epn.edu.ec>
2024-07-24 22:14:09 -0500

    Primer commit: implementación de ventanas

---
+++ src/Lider.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Lider {
+    private JTextPane MenuLider;
+    private JButton AsignarTareas;
+    private JButton AgregarMiembro;
+    private JButton VerProgreso;
+    public JPanel PanelLider;
+    private JButton FinSesion;
+}
---
+++ src/Login.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+public class Login {
+    private JTextPane TituloLogin;
+    private JTextArea Usuario;
+    private JTextArea Clave;
+    private JTextField IngresoUsuario;
+    private JTextField IngresoClave;
+    private JButton IniciarSesion;
+    private JTextArea NuevosUsuarios;
+    private JComboBox SeleccionUsuario;
+    private JTextArea TipoUsuario;
+    private JButton Registrarse;
+    private JLabel MensajeLogin;
+    public JPanel PanelLogin;
+
+}
