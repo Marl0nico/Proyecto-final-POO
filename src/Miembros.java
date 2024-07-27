147d4c58e41556e2c7b7eb5e0907eea89a7c1132
Marl0nico <marlon.nicolalde@epn.edu.ec>
2024-07-27 12:05:55 -0500

    Commit 2: conexión con la BD

---
+++ src/Miembros.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Miembros {
+    public JPanel PanelMiembros;
+    private JLabel Miembros;
+    private JButton AsignarTarea;
+    private JButton FinSesion;
+    private JButton ActualizarTarea;
+}
